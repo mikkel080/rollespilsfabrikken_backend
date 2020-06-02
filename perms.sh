008133b795aac728dce1191826e287890d043676
root <root@Arn-Server>
2020-06-02 21:06:23 +0100

    Role without delete

---
+++ perms.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd ..
+
+echo "Chowning to www-data"
+
+sudo chown -R www-data:www-data rollespilsfabrikken_backend/
+sudo chown -R www-data:www-data rollespilsfabrikken_backend
+
+echo "Setting file permissions"
+sudo find rollespilsfabrikken_backend/ -type f -exec chmod 644 {} \;
+sudo find rollespilsfabrikken_backend -type f -exec chmod 644 {} \;
+
+echo "Setting directory permissions"
+sudo find rollespilsfabrikken_backend/ -type d -exec chmod 755 {} \;
+sudo find rollespilsfabrikken_backend -type d -exec chmod 755 {} \;
+
+echo "Set read write on storage and cache"
+cd rollespilsfabrikken_backend
+sudo chgrp -R www-data storage bootstrap/cache
+sudo chmod -R ug+rwx storage bootstrap/cache
+
+echo "Done"
