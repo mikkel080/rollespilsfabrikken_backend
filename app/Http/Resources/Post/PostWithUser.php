0bbea9564c8e0b65f71157f30375d0e6da916bcd
albe2669 <albert@risenielsen.dk>
2020-06-23 21:22:42 +0200

    Formatting

--- app/Http/Resources/Post/PostWithUser.php
+++ app/Http/Resources/Post/PostWithUser.php
@@ -26,7 +26,7 @@ class PostWithUser extends JsonResource
             'pinned' => $this->pinned,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-	    'comments' => $this->comments()->count(),
+	        'comments' => $this->comments()->count(),
             'files' => $this->when($this->files, PostFileResource::collection($this->files)),
             'permissions' => [
                 'can_update' => auth()->user()->can('update', $this->resource),
