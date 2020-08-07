75e0ee6a1c6eb85b714cedeb1f9d26930a72a40c
albe2669 <albert@risenielsen.dk>
2020-08-07 15:33:21 +0200

    Documented UserRole properties

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 /**
  * Class UserRole
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $role_id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ *
  * @mixin Builder
  */
 class UserRole extends Model
