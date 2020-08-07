967ea0da038490d251fba81c90f62c8ee6df5643
albe2669 <albert@risenielsen.dk>
2020-08-07 14:55:40 +0200

    Documented EventMeta properties

--- app/Models/EventMeta.php
+++ app/Models/EventMeta.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 /**
- * Class Event
+ * Class EventMeta
+ *
+ * @property int $id
+ * @property string $event_id
+ * @property int $repeat_start
+ * @property int $repeat_interval
+ * @property int $repeat_end
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ *
  * @mixin Builder
  */
 class EventMeta extends Model
